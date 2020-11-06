274c270ac69d6f607af681333b1a9a8b84887aca
Liszt21 <1832666492@qq.com>
2020-11-06 16:51:16 +0800

    create QuantifiedSelf

--- frontend/src/views/Home.tsx
+++ frontend/src/views/Home.tsx
@@ -1,11 +1,50 @@
 import React from "react";
+import { dateFormat } from "../utils";
 
+// interface IRecord {
+// 	start: Date;
+// 	end: Date;
+// 	tags: string[];
+// 	detail?: string;
+// 	location?: ILocation[];
+// }
+
+// interface ILocation {
+// 	longitude: number;
+// 	latitude: number;
+// }
+
+interface IQSState {
+	now: Date;
+}
+
+interface IQSProps {}
+
+interface IQSTimeProps {
+	time: Date;
+}
 class QuantifiedSelf extends React.Component {
+	state: IQSState;
+	interval: number;
+	constructor(props: IQSProps) {
+		super(props);
+		this.state = { now: new Date() };
+		this.interval = setInterval(() => {this.setState({ now: new Date() });}, 100);
+	}
 	render() {
 		return (
-			<p>Quantified self</p>
+			<div><QSTime time={this.state.now}/></div>
 		)
 	}
+	componentWillUnmount() {
+		clearInterval(this.interval);
+	}
+}
+
+function QSTime(props: IQSTimeProps) {
+	return (
+		<p>{dateFormat(props.time)}</p>
+	)
 }
 
 function Home() {
