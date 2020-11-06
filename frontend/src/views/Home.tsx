7f415f8016086f47f3f8dc02e7a5c1f7dafdb7bf
Liszt21 <1832666492@qq.com>
2020-11-06 17:15:20 +0800

    add EQSAction

--- frontend/src/views/Home.tsx
+++ frontend/src/views/Home.tsx
@@ -23,18 +23,35 @@ interface IQSProps {}
 interface IQSTimeProps {
 	time: Date;
 }
+
+interface IQSActionProps {
+	action: Function;
+}
+
+enum EQSAction {
+	start = 1,
+	stop,
+	pause,
+	cancel
+}
 class QuantifiedSelf extends React.Component {
 	state: IQSState;
-	interval: number;
+	interval?: number;
 	constructor(props: IQSProps) {
 		super(props);
 		this.state = { now: new Date() };
-		this.interval = setInterval(() => {this.setState({ now: new Date() });}, 100);
 	}
 	render() {
 		return (
-			<div><QSTime time={this.state.now}/></div>
+			<div>
+				<QSTime time={this.state.now}/>
+				<QSAction action={(action: EQSAction) => console.log(EQSAction[action])}
+					/>
+			</div>
 		)
+	}
+	componentDidMount() {
+		this.interval = setInterval(() => {this.setState({ now: new Date() });}, 100);
 	}
 	componentWillUnmount() {
 		clearInterval(this.interval);
@@ -44,6 +61,17 @@ class QuantifiedSelf extends React.Component {
 function QSTime(props: IQSTimeProps) {
 	return (
 		<p>{dateFormat(props.time)}</p>
+	)
+}
+
+function QSAction(props: IQSActionProps) {
+	return (
+		<div>
+			<button onClick={() => props.action(1)}>Start</button>
+			<button onClick={() => props.action(2)}>Stop</button>
+			<button onClick={() => props.action(3)}>Pause</button>
+			<button onClick={() => props.action(4)}>Cancel</button>
+		</div>
 	)
 }
 
