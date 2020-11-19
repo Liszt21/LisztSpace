05c0335584991b7c07b6d85b2a68202e53b57c06
Liszt21 <1832666492@qq.com>
2020-11-19 11:50:37 +0800

    wrap axios by useEffect

--- frontend/src/views/Tools.tsx
+++ frontend/src/views/Tools.tsx
@@ -1,16 +1,19 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 
 function Tools() {
   const [ ping, setPing ] = useState("Ping");
-  axios.get("http://localhost:5000/api/ping")
-    .then(function (response) {
-      setPing(response.data);
-      console.log(response);
-    })
-    .catch(function (error) {
-      console.log(error);
-    });
+  useEffect(()=>{
+    axios.get("http://localhost:5000/api/ping")
+      .then(function (response) {
+        setPing(response.data);
+        console.log(response);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  }, [])
+  
   return (
       <p>{ping}</p>
   )
