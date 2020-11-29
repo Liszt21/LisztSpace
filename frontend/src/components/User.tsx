26ff899d1ae2d6e42d075f0f4368e3cbbd4fcaf3
Liszt21 <1832666492@qq.com>
2020-11-30 00:20:28 +0800

    test axios basic auth

--- frontend/src/components/User.tsx
+++ frontend/src/components/User.tsx
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { useState } from "react";
 import { Form, Input, Button, Checkbox } from "antd";
+import axios from "axios";
 
 interface UserInfo {
   username: string,
@@ -7,8 +8,18 @@ interface UserInfo {
 }
 
 function Signin() {
+  const [ username, setUsername ] = useState("please Sign in")
   const onFinish = (values: UserInfo)=> {
     console.log('Success:', values);
+    axios.get('http://localhost:5000/api/user/1', {
+      auth: {
+        username: values.username,
+        password: values.password
+      }
+    }).then(result => {
+      setUsername(result.data.username)
+    })
+
   };
 
   const onFinishFailed = (errorInfo: Object)=> {
@@ -16,38 +27,41 @@ function Signin() {
   };
 
   return (
-    <Form
-      name="basic"
-      initialValues={{ remember: true }}
-      onFinish={onFinish}
-      onFinishFailed={onFinishFailed}
-    >
-      <Form.Item
-        label="Username"
-        name="username"
-        rules={[{ required: true, message: 'Please input your username!' }]}
+    <div>
+      <Form
+        name="basic"
+        initialValues={{ remember: true }}
+        onFinish={onFinish}
+        onFinishFailed={onFinishFailed}
       >
-        <Input />
-      </Form.Item>
+        <Form.Item
+          label="Username"
+          name="username"
+          rules={[{ required: true, message: 'Please input your username!' }]}
+        >
+          <Input />
+        </Form.Item>
 
-      <Form.Item
-        label="Password"
-        name="password"
-        rules={[{ required: true, message: 'Please input your password!' }]}
-      >
-        <Input.Password />
-      </Form.Item>
+        <Form.Item
+          label="Password"
+          name="password"
+          rules={[{ required: true, message: 'Please input your password!' }]}
+        >
+          <Input.Password />
+        </Form.Item>
 
-      <Form.Item name="remember" valuePropName="checked">
-        <Checkbox>Remember me</Checkbox>
-      </Form.Item>
+        <Form.Item name="remember" valuePropName="checked">
+          <Checkbox>Remember me</Checkbox>
+        </Form.Item>
 
-      <Form.Item>
-        <Button type="primary" htmlType="submit">
-          Submit
-        </Button>
-      </Form.Item>
-    </Form>
+        <Form.Item>
+          <Button type="primary" htmlType="submit">
+            Submit
+          </Button>
+        </Form.Item>
+      </Form>
+      <p>{ username }</p>
+    </div>
   )
 }
 
